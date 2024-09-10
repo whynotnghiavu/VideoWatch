04463b50b7a006b6b3b88f589e597a47fdc2ad5f
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-10 10:23:38 +0700

    update Button.py, THEMES.py and UseCase.py

--- contents/code/view/Button.py
+++ contents/code/view/Button.py
@@ -4,14 +4,17 @@ from view.THEMES import THEMES
 
 class Button():
     button_themes = {
-        UseCase.XoaManHinh: THEMES.DOC_LAP,
+        UseCase.XoaManHinh: THEMES.DRACULA,
 
+        UseCase.ChonThuMuc: THEMES.DRACULA,
 
+        UseCase.MoVideo: THEMES.DARK_BLUE,
+        UseCase.MoGhiChu: THEMES.DARK_BLUE,
 
+        UseCase.XemVideoTruoc: THEMES.DRACULA,
+        UseCase.XemVideoSau: THEMES.DARK_BLUE,
 
-
-
-
+        UseCase.MoTatCaGhiChu: THEMES.DRACULA,
 
         UseCase.ThoatChuongTrinh: THEMES.EXIT,
     }
--- contents/code/view/UseCase.py
+++ contents/code/view/UseCase.py
@@ -1,7 +1,15 @@
 class UseCase():
     XoaManHinh = "Xóa màn hình"
 
-    # ChonThuMuc = "Chọn thư mục"
+    ChonThuMuc = "Chọn thư mục"
+
+    MoVideo = "Mở Video"
+    MoGhiChu = "Mở ghi chú"
+
+    XemVideoTruoc = "Xem Video trước"
+    XemVideoSau = "Xem Video sau"
+
+    MoTatCaGhiChu = "Mở tất cả ghi chú"
 
     ThoatChuongTrinh = "Thoát chương trình"
     error = "Không có chức năng này"
