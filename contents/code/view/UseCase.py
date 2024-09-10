2bde50cd495b40f8300a696e427e64fd47f66e15
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-10 09:54:52 +0700

    update Button.py and UseCase.py

--- contents/code/view/UseCase.py
+++ contents/code/view/UseCase.py
@@ -1,46 +1,10 @@
 class UseCase():
-    # KiemTraAmThanh = "Kiểm tra âm thanh"
-    # XoaManHinh = "Xóa màn hình"
+    XoaManHinh = "Xóa màn hình"
 
     # ChonThuMuc = "Chọn thư mục"
-    # GhiThongTin = "Ghi thông tin"
-
-    # TatCa1 = "Tất cả 1"
-    # LuuCauTrucThuMuc = "Lưu cấu trúc thư mục"
-    # SaoChepThuMuc = "Sao chép thư mục"
-
-    # XoaFileKhongDung = "Xóa file không dùng"
-
-    # DoiTenFolderCon = "Đổi tên folder con"
-    # DoiTenFileCon = "Đổi tên file con"
-    # KiemTraTenGiongNhau = "Kiểm tra tên giống nhau"
-    # TaoFileSubRieng = "Tạo file sub riêng"
-    # HopNhatCauSub = "Hợp nhất câu sub"
-    # HopNhatTatCaFileSub = "Hợp nhất tất cả file sub"
-
-    # DichTiengViet = "Dịch tiếng Việt"
-
-    # TatCa2 = "Tất cả 2"
-
-    # XuLyFileKetQuaDich = "Xử lý file kết quả dịch"
-
-    # TachFileSub = "Tách file sub"
+    
 
 
-
-
-    # TatCa3 = "Tất cả 3"
-
-
-
-    # ChuyenAmThanh = "Chuyển âm thanh"
-    # ThemAmThanh = "Thêm âm thanh"
-    # HopAmThanh = "Hợp âm thanh"
-    # GhepAmThanh = "Ghép âm thanh"
-    # DonDep = "Dọn dẹp"
-    # DoiTenFileSub = "Đổi tên file sub"
-
-    # XoaFileSub = "Xóa file sub"
 
 
 
