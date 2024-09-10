b61998ef1018dc68f0721cc47aae4b3c98665c94
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-10 09:49:04 +0700

    create 5 files and update 1 file

---
+++ contents/code/view/THEMES.py
@@ -0,0 +1,8 @@
+class THEMES:
+    DOC_LAP = ("#00d1b2", "#000000")
+    CHON_THU_MUC = ("#ff8f1c", "#000000")
+    BINH_THUONG = ("#282a36", "#bd93f9")
+    TAT_CA = ("#0f4c81", "#ffffff")
+    DAC_BIET = ("#abd200", "#000000")
+    EXIT = ("#800000", "#d4af37")
+# chỉnh  json {background , foreground} tránh 0,1  list []
